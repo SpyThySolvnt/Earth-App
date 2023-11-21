a29213dc3208faba1a982813c5d8c91f0c2f56c8
Immortalx0 <algharbi2001@gmail.com>
2023-11-21 12:33:53 +0400

    Admin Class

---
+++ Admin.java
@@ -0,0 +1,19 @@
+
+
+public class Admin {
+    private String name;
+    private String id;
+    private String password;
+
+    public Admin(String name, String id, String password) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    
+    
+    }
+}
+
+
+
+
