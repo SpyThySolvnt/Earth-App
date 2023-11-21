4909a8a4240c5b30854bbbcf51b1acd023f4af83
SpyThySolvnt <albadi999@gmail.com>
2023-11-21 20:27:10 +0400

    Create AdminFrame.java

---
+++ AdminFrame.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+public class AdminFrame extends JFrame{
+
+
+
+
+
+
+
+
+
+}
