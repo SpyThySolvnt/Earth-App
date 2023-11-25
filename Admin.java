e7e8e272282c57ec11eb37b0d057619e6fe932e1
Immortalx0 <algharbi2001@gmail.com>
2023-11-25 16:37:52 +0400

    update admin Class
    should run now

--- Admin.java
+++ Admin.java
@@ -1,27 +1,17 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
-// Admin class
 public class Admin {
-    // Attributes
     private String name;
     private String id;
     private String password;
 
-    private Initiative[] initiatives;  // Array to store initiatives
-    private Initiator[] initiators;    // Array to store initiators
-    private Volunteer[] volunteers;    // Array to store volunteers
-
-    // Constructor
     public Admin(String name, String id, String password) {
         this.name = name;
         this.id = id;
         this.password = password;
-        this.pendingInitiatives = new ArrayList<>();
     }
 
-    // Getter methods for attributes 
     public String getName() {
         return name;
     }
@@ -29,70 +19,57 @@ public class Admin {
     public String getId() {
         return id;
     }
-    
+
     public String getPassword() {
         return password;
     }
-    
 
-    // Admin login method
     public boolean login(String enteredPassword) {
         return password.equals(enteredPassword);
     }
 
-    // Approve or reject an initiative
+    // High-level operations for managing initiatives
     public void processInitiative(Initiative initiative, boolean approve) {
-        if (approve) {
-            initiative.setStatus("active");
-        } else {
-            initiative.setStatus("rejected");
-            pendingInitiatives.remove(initiative);
-        }
-    }
-    // Remove initiative, initiator, or volunteer
-    public void removeRecord(Object record) {
-        if (record instanceof Initiative) {
-            removeInitiative((Initiative) record);
-        } else if (record instanceof Initiator) {
-            removeInitiator((Initiator) record);
-        } else if (record instanceof Volunteer) {
-            removeVolunteer((Volunteer) record);
+        if (initiative != null) {
+            initiative.process(approve);
         }
     }
 
-    private void removeInitiative(Initiative initiative) {
-        // Implementation to remove an initiative from the array
-        for (int i = 0; i < initiatives.length; i++) {
-            if (initiatives[i].equals(initiative)) {
-                // Remove the initiative by shifting elements
-                System.arraycopy(initiatives, i + 1, initiatives, i, initiatives.length - 1 - i);
-                // Set the last element to null or create a new array with reduced size
-                initiatives[initiatives.length - 1] = null;
-                // Write updated initiatives array back to the file or database
-                writeInitiativesToFile();
-                break;
-            }
-        }
+    public void removeInitiative(Initiative initiative) {
+        // Implementation to remove initiative from a centralized list
+        // You can implement this based on your design
     }
 
-    // View all initiatives in tabular form
     public void viewAllInitiatives() {
-        // Implementation to display all initiatives in a tabular form
+        // Implementation to view all initiatives
+        // You can implement this based on your design
     }
 
-    // Search for an initiative or volunteer by name
-    public void searchByName(String name) {
-        // Implementation to search for an initiative or volunteer by name
-    }
+    // Other high-level operations for managing initiators and volunteers
 
-    // View details of an initiative, initiator, or volunteer
+    // Example method to view details of any record
     public void viewDetails(Object record) {
-        // Implementation to display details of a record
+        if (record != null) {
+            System.out.println(record); // Assuming toString() is overridden in respective classes
+        }
     }
-    
 }
 
+class Initiative {
+    // Implementation for Initiative class
+    // ...
 
+    public void process(boolean approve) {
+        // Specific implementation for processing an initiative
+    }
+}
 
+class Initiator {
+    // Implementation for Initiator class
+    // ...
+}
 
-
+class Volunteer {
+    // Implementation for Volunteer class
+    // ...
+}
