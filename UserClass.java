f64ec23c3a4944acdc28dc9faadb44a506b2979c
GhaithAlqaydi <151761559+GhaithAlqaydi@users.noreply.github.com>
2023-11-26 20:26:03 +0400

    Update UserClass.java

--- UserClass.java
+++ UserClass.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
 public class UserClass {
     // Attributes
@@ -12,69 +12,54 @@ public class UserClass {
     private String password;
     private List<Initiative> registeredInitiatives;
     private List<Initiative> volunteeringJobs;
-    ArrayList<Admin> adminusers = new ArrayList<Admin>();
+    protected  static ArrayList<UserClass> allUL = new ArrayList<>();
+    private String Type[] = {"Volunteer","Initiator","Volunteer and Initiator"};
+    private String FinalType ;
     
-
-
-     public UserClass() {
-    	   id = generateUserId(); 
-    }// Empty Constructor But it has an ID
+    PrintWriter foutU = new PrintWriter(new FileOutputStream(new File("C:\\Users\\3R\\Desktop\\FileOfUsers.txt"),true));
+    PrintWriter foutV = new PrintWriter(new FileOutputStream(new File("C:\\Users\\3R\\Desktop\\FileOfVolunteer.txt"),true));
+    PrintWriter foutI = new PrintWriter(new FileOutputStream(new File("C:\\Users\\3R\\Desktop\\FileOfInitiator.txt"),true));
     
-    //Constructor for initiative
-    public UserClass(String name) {
-    	 this.name = name;
-         id = generateUserId(); 
-    }
     // Constructor
-    public UserClass(String name, String password, String phoneNumber, String email , String  address, String  dateOfBirth) {
-        this.name = name;
+    public UserClass(String FinalType, String name, String password, String phoneNumber, String email , String  address, String  dateOfBirth) throws FileNotFoundException{
+        this.FinalType = FinalType;
+    	this.name = name;
         this.id = generateUserId(); // You need to implement this method
         this.email = email;
         this.dateOfBirth = dateOfBirth;
         this.phoneNumber = phoneNumber;
         this.address = address;
         this.password = password;
-
         this.registeredInitiatives = new ArrayList<>();
         this.volunteeringJobs = new ArrayList<>();
+        foutU.println(this);
+      foutU.close();
+      if (FinalType.equals("Volunteer")) {
+    	  foutV.println(this);
+          foutV.close();
+      } else if (FinalType.equals("Initiator")) {
+    	  foutI.println(this);
+          foutI.close();
+      } else if (FinalType.equals("Volunteer and Initiator")) {
+    	  foutV.println(this);
+          foutV.close();
+          foutI.println(this);
+          foutI.close();
+      }
     }
 
-    // Getter methods for attributes
-    public String getName() {
-        return name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    // User sign up method
-    public void signUp() {
-        // Implementation to sign up a new user
-    }
+//    public void generateVolunteerReport() throws FileNotFoundException {
+//    	String flocation = getName()+""+getId()+".txt";
+//    	PrintWriter v2fout = new PrintWriter(flocation);
+//    	v2fout.println(" hi ");
+//    	v2fout.close();
+//    	// needs to print the credits and other details, not done
+//    	// Implementation to generate a report of the user's volunteering history and points
+//     }
+    
 
     // User login method
-    public boolean login(String enteredPassword) {
+    public boolean login(String name, String enteredPassword) {
         return password.equals(enteredPassword);
     }
 
@@ -87,13 +72,7 @@ public class UserClass {
     }
 
     // Initiator-specific methods
-    public void addInitiative(Initiative initiative) {
-        // Implementation to add a new initiative by the user
-    }
-
-    public void editInitiative(Initiative initiative) {
-        // Implementation to edit an existing initiative by the user
-    }
+   
 
     public void deleteInitiative(Initiative i) {
       i.idOfEvent= null;
@@ -104,11 +83,7 @@ public class UserClass {
       i.placeOfEvent=null;
       i.credit= 0;
     }
-
-    public List<Initiative> viewRegisteredVolunteers() {
-        // Implementation to view all registered volunteers for the user's initiatives
-        return new ArrayList<>();
-    }
+   
 
     // Volunteer-specific methods
     public List<Initiative> viewAllActiveInitiatives() {
@@ -129,5 +104,44 @@ public class UserClass {
     	id = UUID.randomUUID().toString();
 		return id
 				;
+    }
+    
+	  public String toString() {
+	        return "(" + FinalType +
+	                " ID ='" + getId() + ") (" +
+	                " Name ='" + getName() + ") (" +
+	                " Date-Of-Birth ='" + getDateOfBirth() + ") (" +
+	                " Email ='" + getEmail() + '\'' +
+	                " Phone-Number ='" + getPhoneNumber() + ") (" +
+	                " Address ='" + getAddress() + ") (" +
+	                " Password ='" + getPassword() + ") (" +
+	                " Volunteering-Jobs =" + volunteeringJobs+ ")\n";
+	    }
+    
+ // Getter methods for attributes
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPassword() {
+        return password;
+    }
+    // User sign up method
+    public void signUp() {
+        // Implementation to sign up a new user
     }
 }
