a2d580a45bf3007ff8463ee70620edbe6b7cbc10
GhaithAlqaydi <151761559+GhaithAlqaydi@users.noreply.github.com>
2023-11-27 18:55:05 +0400

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Database{
+String Data;
+public static ArrayList<User> UserList = new ArrayList<User>();
+public static ArrayList<Initiative> EventList = new ArrayList<Initiative>();
+
+public Database(String Data) {
+	this.Data = Data;
+}
+}
