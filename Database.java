309e6aa06042a293ab4a38757aebe98135ede56c
GhaithAlqaydi <151761559+GhaithAlqaydi@users.noreply.github.com>
2023-11-27 22:35:07 +0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -2,11 +2,18 @@ import java.io.*;
 import java.util.*;
 
 class Database{
-String Data;
+public String Data;
 public static ArrayList<User> UserList = new ArrayList<User>();
+
 public static ArrayList<Initiative> EventList = new ArrayList<Initiative>();
 
 public Database(String Data) {
 	this.Data = Data;
+	
 }
+
+
+
+
 }
+
