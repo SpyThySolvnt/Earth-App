19e386ff712e314ff1c49fb52491eed2802e58d8
Hameddd <151604841+HamedAlDarmaki@users.noreply.github.com>
2023-11-28 08:51:03 +0400

    Update AdminFrame.java

--- AdminFrame.java
+++ AdminFrame.java
@@ -5,39 +5,61 @@ import java.io.*;
 import java.util.*;
 import javax.swing.*;
 
+
+
 public class AdminFrame extends JFrame{
 
-
+     String admin;
+     String admin2023;
     JTextField username;
-    JPasswordField password;
-    JCheckBox Nike;
+    JTextField password;
+   // JTextField id;
     Container c;
     JButton Enter;
-
-
-
+    JButton Back;
 
     private class ALenter implements ActionListener{
         public void actionPerformed(ActionEvent e){     
-            AdminPage x = new AdminPage();
-            dispose();
+          
+            if(e.getSource()==Enter) {
+            	
+            	
+            	if(username.getText().equals("admin")||password.getText().equals("admin2023")){
+            		JOptionPane.showMessageDialog(null , "Welcome Back Admin",null,JOptionPane.PLAIN_MESSAGE); 
+            		AdminPage x = new AdminPage();
+                    dispose();
+                	
+            	}
+            	
+            	else      	   JOptionPane.showMessageDialog(null,"Wrong name or password!","error",JOptionPane.ERROR_MESSAGE);
 
+        } 
         }
     }
-    private class ALnike implements ActionListener{
-        public void actionPerformed(ActionEvent e){
-            if(Nike.isSelected()){
-            password.setEchoChar((char)0);}
-            else{
-            password.setEchoChar('*');}
+    
+    private class ALback implements ActionListener{
+        public void actionPerformed(ActionEvent e){ 
+            Hamdan x = new Hamdan(); 
+            dispose();   
+            
+            if(e.getSource()==Enter) {
+                String jtxtnum = password.getText();
+                if(jtxtnum==admin2023 && username.getText()==admin) {
+            
+            		JOptionPane.showMessageDialog(null , "Welcome back admin",null,JOptionPane.PLAIN_MESSAGE); 
+             }
+                
+                
+                }
         }
-    }
-
+            
+        }
     AdminFrame(){
 
-        //A must on Jfranes
+        //A must on Jframes
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         c = getContentPane();
         c.setLayout(null);
         setSize(500,300);
@@ -51,33 +73,32 @@ public class AdminFrame extends JFrame{
         username.setBounds(200,50,150,50);
         JLabel Pass = new JLabel("Password:");
         Pass.setBounds(100,100,150,50);
-        password = new JPasswordField();
+        password = new JTextField();
         password.setBounds(200,100,150,50);
         Enter = new JButton("Enter");
         Enter.setBounds(100,210,300,50);
-        Nike = new JCheckBox("Show Password");
-        Nike.setBounds(200,150,150,50);
+        Back = new JButton("Back");
+        Back.setBounds(0,0,100,30);
        // JLabel Id = new JLabel("ID :");
        // Id.setBounds(100,200,150,50);
         //id = new JTextField();
        // id.setBounds(200,200,150,50);
-        
-
-
+  
         //Buttons
         ALenter enButton = new ALenter();
         Enter.addActionListener(enButton);
-        ALnike k = new ALnike();
-        Nike.addActionListener(k);  
+        
+        ALback s = new ALback();
+        Back.addActionListener(s);
+        
+      
 
+        
 
-
-        //A must on Jfranes
+        //A must on Jframes
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-
-
 
         //adding assests
         c.add(User);
@@ -85,18 +106,12 @@ public class AdminFrame extends JFrame{
        // c.add(Id);
         c.add(username);
         c.add(password);
+        c.add(Back);
         //c.add(id);
         c.add(Enter);
-        c.add(Nike);
-
-
-        setLocationRelativeTo(null);
 
 
     }
 
 
-
-
-
-}
+    }
