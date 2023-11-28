61efd15d9878ab6a563a39e8d4d94fb765ccff2d
Hameddd <151604841+HamedAlDarmaki@users.noreply.github.com>
2023-11-28 08:28:07 +0400

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -10,6 +10,7 @@ public class LoginPage extends JFrame{
 
     JButton Initiative;
     JButton Settings;
+    JButton volunteer;
     JTextArea Place;
     
     Container c;
@@ -33,6 +34,14 @@ public class LoginPage extends JFrame{
 
         }
     }
+    private class ALvolunteer implements ActionListener{
+        public void actionPerformed(ActionEvent e){     
+            VolunteerFrame v = new VolunteerFrame();
+            dispose();
+            
+
+        }
+    }
     
 
 
@@ -43,7 +52,7 @@ public class LoginPage extends JFrame{
 
     LoginPage(){
 
-    //A must on Jfranes
+    //A must on Jframes
     setVisible(true);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     c = getContentPane();
@@ -55,6 +64,15 @@ public class LoginPage extends JFrame{
     
     JLabel initiative = new JLabel ("Welcome to the Earth App Here you can Volunteer and Create Initiative to help save the world bit by bit!");
     initiative.setBounds(150,0,600,150);
+
+    
+
+
+    
+
+
+
+
     //buttons
 
     Initiative = new JButton("Create An Initiative");
@@ -62,17 +80,24 @@ public class LoginPage extends JFrame{
     Settings = new JButton("Settings");
     Settings.setBounds(0,580,150,80);
 
+    volunteer = new JButton("VOLUNTEER");
+    volunteer.setBounds(300,550,300,30);
+    
+    
+    
     ALinitiative Ini = new ALinitiative();
     Initiative.addActionListener(Ini);
     ALsettings sett = new ALsettings();
     Settings.addActionListener(sett);
-
+    ALvolunteer vol = new ALvolunteer();
+    volunteer.addActionListener(vol);
 
 
     //adding assests
     c.add(initiative);
     c.add(Settings);
     c.add(Initiative);
+    c.add(volunteer);
 
 
 
