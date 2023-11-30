704ee3738768e956f94bef4922be11e193a8c7e3
SpyThySolvnt <albadi999@gmail.com>
2023-11-30 17:20:07 +0400

    Update AdminPage.java

--- AdminPage.java
+++ AdminPage.java
@@ -11,6 +11,7 @@ public class AdminPage extends JFrame{
 
     JButton Approve;
     JTextArea Initiatives;
+    JTable z;
     Container c;
 
 
@@ -36,12 +37,43 @@ public class AdminPage extends JFrame{
 
 
      //Jtable 
+     String column [] = {"Counter","ID "," Initiative","Description","Credit Hour","Time","Date"};
+
+     String row [][] = {{"Dino1", "Dino2","Dino3","Dino4","Dino 5","Dino6","Dino7"},
+                         {"Bear 1", "Bear2","Bear3","Bear4","Bear 5","Bear6","Bear7"},
+                     {"Moose 1", "Moose2","Moose3","Moose4","Moose 5","Moose6","Moose7"},
+                     {"Rabbit 1", "Rabbit2","Rabbit3","Rabbit4","Rabbit 5","Rabbit 6","Rabbit 7"},
+                     {"dog 1", "dog2","dog3","dog4","dog 5","dog6","dog 7"},
+                     {"cat 1", "cat 2","cat3","cat4","cat 5","cat 6","cat 7"},
+                     {"wolf 1", "wolf2","wolf3","wolf4","wolf 5","wolf 6","wolf 7"},
+                 
+                 
+                 
+                 
+                 };
+ 
+ 
+ DefaultTableModel model = new DefaultTableModel( row,  column) {
+ 
+ 
+ public boolean isCellEditable(int row, int column) {
+     return false;
+     }
+   };
+                 
+ 
+     
+     z = new JTable(model);
+     JScrollPane y = new JScrollPane(z);
+     y.setBounds(200,200,500,100);
+     z.getTableHeader().setReorderingAllowed(false);
+     z.getTableHeader().setResizingAllowed(false);
  
 
 
     c.add(Welcome);
     //c.add(Initiatives);
-    //c.add(x);
+    c.add(y);
 
     //for some reason this needs to be in the end
     setLocationRelativeTo(null);
